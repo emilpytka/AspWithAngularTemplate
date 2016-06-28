5d6a40a7027eaf78b302f16773ec4197939ada7c
Emil Pytka <emilpytka@gmail.com>
2016-06-28 21:47:32 +0200

    remove unused code

--- AspWithAngularTemplate/app/main.ts
+++ AspWithAngularTemplate/app/main.ts
@@ -1,6 +1,5 @@
 ﻿/// <reference path="../typings/index.d.ts" />
 
-import { XHRBackend } from '@angular/http';
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { provide } from '@angular/core';
 
@@ -9,11 +8,7 @@ import { AppComponent } from './app.component';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { HTTP_PROVIDERS } from '@angular/http';
 
-import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
-import { InMemoryDataService }               from './in-memory-data.service';
-
 import { ValuesService } from './values.service';
-
 
 import {
     APP_BASE_HREF,
